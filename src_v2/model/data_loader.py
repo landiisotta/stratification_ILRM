9ebd6543cf48efa03bc68febd01a90621dfe0a9e
Riccardo Miotto <riccardo.miotto@mssm.edu>
2019-02-15 17:29:18 -0500

    new data loader

--- src_v2/model/data_loader.py
+++ src_v2/model/data_loader.py
@@ -24,37 +24,45 @@ class EHRdata(Dataset):
         with open(os.path.join(datadir, ehr_file)) as f:
             rd = csv.reader(f)
             for r in rd:
-                seq = list(map(int, r[1::]))
+                seq = map(int, r[1::])
 
                 if len(seq) < padded_seq_len:
                     self.ehr[r[0]] = seq + [0] * \
                         (padded_seq_len - len(seq))
 
                 elif len(seq) % padded_seq_len != 0:
-                    self.ehr[r[0]] = seq + [0] * \
+                    pad_seq = seq + [0] * \
                         (padded_seq_len - len(seq) % padded_seq_len)
+                    sq = []
+                    for idx in range(0, len(pad_seq) - padded_seq_len + 1, padded_seq_len):
+                        sq.append(pad_seq[idx:idx + padded_seq_len])
 
                 else:
                     self.ehr[r[0]] = seq
 
+        self.ehr_list = [[mrn, term] for mrn, term in self.ehr.items()]
+
 
     def __getitem__(self, index):
-        ehr_list = [[mrn, term] for mrn, term in self.ehr.items()]
-        seq = ehr_list[index][1]
-        pat = ehr_list[index][0]
-        return seq, pat
-
+        # ehr_list = [[mrn, term] for mrn, term in self.ehr.items()]
+        seq = self.ehr_list[index][1]
+        pat = self.ehr_list[index][0]
+        return (seq, pat)
 
     def __len__(self):
         return len(self.ehr)
 
 
 def ehr_collate(batch):
+    print len(batch)
     data = []
     mrn = []
     for seq, pat in batch:
         mrn.append(pat)
+        data.append(torch.tensor(
+                [seq], dtype=torch.long).view(-1, padded_seq_len))
 
+        """
         if len(seq) == padded_seq_len:
             data.append(torch.tensor(
                 [seq], dtype=torch.long).view(-1, padded_seq_len))
@@ -70,5 +78,6 @@ def ehr_collate(batch):
             raise Warning(
                 'Not all the sequences have length multiple than {0}'.format(
                     padded_seq_len))
+        """
 
     return [data, mrn]
