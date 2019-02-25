4092b9c2366c9363b761e9912f2599c8e06904a4
Riccardo <riccardo.miotto@mssm.edu>
2019-02-25 17:00:53 -0500

    server experiments

--- src_v2/model/net.py
+++ src_v2/model/net.py
@@ -68,9 +68,10 @@ class ehrEncoding(nn.Module):
         # two layers of encoding
         out = self.linear1(out)
         out = F.dropout(out)
-        out = self.softplus(out)
+        # out = self.softplus(out)
+        # out = self.sigm(out)
         # out = self.linear2(out)
-        # out = F.relu(out)
+        out = F.relu(out)
 
         # encoded representation
         encoded_vect = out.view(-1, out.shape[1])
@@ -79,7 +80,7 @@ class ehrEncoding(nn.Module):
         # out = self.linear3(out)
         # out = F.relu(out)
         out = self.linear4(out)
-        out = self.relu(out)
+        out = F.relu(out)
 
         out = out.view(-1, self.vocab_size, x.shape[1])
 
